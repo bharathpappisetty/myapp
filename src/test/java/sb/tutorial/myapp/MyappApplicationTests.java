39faea86625e875dc89d10b150bbd54231dcc165
LAPTOP-1P9SFLMH\bhara <bharath.pappisetty@gmail.com>
2024-02-29 22:48:40 +0530

    myapp - git repo create.

---
+++ src/test/java/sb/tutorial/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.tutorial.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
